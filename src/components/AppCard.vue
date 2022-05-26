fc1f46a5d00e21c2e1ec2e5e9530c9a7c3b94e8e
tahakyo <ghazouani1988@email.com>
2022-05-26 18:51:35 +0200

    add vote5stars and milestone3 completed

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,11 +1,18 @@
 <template>
   <li class="object_card text-center pd-1">
     <div class="path_card">
-      <img v-if="imageFound" :src="`https://image.tmdb.org/t/p/w342/${imageFound}`" :alt="cardObject.poster_path">
-      <img v-else src="../assets/img/image-not-found.png" alt="image-not-found">
+      <img
+        v-if="imageFound"
+        :src="`https://image.tmdb.org/t/p/w342/${imageFound}`"
+        :alt="cardObject.poster_path"
+      />
+      <img
+        v-else
+        src="../assets/img/image-not-found.png"
+        alt="image-not-found"
+      />
     </div>
     <div class="content">
-
       <!-- <h3>{{ cardObject.title || cardObject.name }}</h3> with "||= 'or" -->
       <h3>{{ cardObject.title ? cardObject.title : cardObject.name }}</h3>
       <h5>
@@ -17,9 +24,11 @@
         :alt="cardObject.original_language"
       />
       <p v-else>{{ cardObject.original_language }}</p>
-      <p>{{ cardObject.vote_average }}</p>
+      <p>{{ vote5stars }}</p>
+      <p>
+        <i v-for="n in 5" :key="n" class="fa-star" :class="n <= vote5stars ? 'fas' : 'far'"></i>
+      </p>
       <!-- <p >{{ cardObject.overview }}</p> -->
-
     </div>
   </li>
 </template>
@@ -32,7 +41,7 @@ export default {
   },
   data() {
     return {
-      flags: ["en", "fr", "it", "ja", "ru", "de", "ar"],
+      flags: ["en", "fr", "it", "ja", "ru", "de", "ar", "es", "pt"],
     };
   },
   computed: {
@@ -41,20 +50,24 @@ export default {
     },
     imageFound() {
       return this.cardObject.poster_path;
-    }
+    },
+    vote5stars() {
+      return Math.ceil( this.cardObject.vote_average / 2 );
+    },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../components/style/common.scss";
 .object_card {
   width: calc(100% / 4 - 10px);
   margin: 5px;
   border: 1px solid white;
   .path_card {
     min-height: 500px;
-    max-height: 500px; 
-      img {
+    max-height: 500px;
+    img {
       width: 100%;
       overflow: hidden;
       padding: 1rem;
